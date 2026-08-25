b5cabc6bc645d7b8dc20f3e14bd2381e63e85e3d
Ahmed <gamerahmed933@gmail.com>
2026-08-26 02:39:32 +0300

    add validate Request and prettier options

--- packages/common/src/utils/validate-request.ts
+++ packages/common/src/utils/validate-request.ts
@@ -1,8 +1,50 @@
-import {z} from 'zod'
 import { HttpError } from '../Errors/http-error.js'
-import type {NextFunction,Request,Response} from 'express'
-import type{ZodType,ZodError} from 'zod'
+import type { NextFunction, Request, Response } from 'express'
+
+import { AnyZodObject, ZodError } from 'zod/v3'
+
+
+type Schema = AnyZodObject;
+type ParamRecord = Record<string, string>;
+type QueryRecord = Record<string, unknown>;
+
+export interface RequestValidationSchema {
+    body?: Schema;
+    params?: Schema;
+    query?: Schema;
+}
 
 
 
+const formatError = (err: ZodError) => {
+    return err.errors.map((err) => ({
+        path: err.path.join('.'),
+        message: err.message
+    }))
 
+}
+
+export const validateRequest = (schemas: RequestValidationSchema) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        try {
+            if (schemas.body) {
+                const parsedBody = schemas.body.parse(req.body) as unknown;
+                req.body = parsedBody
+            }
+            if (schemas.params) {
+                const parsedParams = schemas.params.parse(req.params) as Request['params'];
+                req.params = parsedParams
+            }
+            if (schemas.query) {
+                const parsedQuery = schemas.query.parse(req.query) as Request['query'];
+                req.query = parsedQuery
+            }
+            next()
+        } catch (err) {
+            if (err instanceof ZodError) {
+               return next(new HttpError(422, err.message))
+            }
+            return err
+        }
+    }
+}
